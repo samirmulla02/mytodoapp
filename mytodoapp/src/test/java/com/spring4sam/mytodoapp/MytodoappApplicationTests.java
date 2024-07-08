646b03aea6a27860e38868cf06f9bafe3e7fd4ba
unknown <samirmulla02@gmail.com>
2024-07-08 22:56:57 +0530

    initial commit

---
+++ mytodoapp/src/test/java/com/spring4sam/mytodoapp/MytodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring4sam.mytodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
